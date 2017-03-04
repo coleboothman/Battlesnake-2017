82c3e32c34e52b2cd900a5417815552cb05adfa4
Cole Boothman <coleboothman@Coles-MacBook-Air.local>
2017-03-04 15:16:57 -0800

    head url

--- app/main.py
+++ app/main.py
@@ -15,7 +15,7 @@ def start():
     board_width = data['width']
     board_height = data['height']
 
-    head_url = '%s://%s/static/head.png' % (
+    head_url = 'http://imgur.com/9xI40bH.png' % (
         bottle.request.urlparts.scheme,
         bottle.request.urlparts.netloc
     )
